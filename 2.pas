6cc2186daa10813c5392d1c2703e71f9a7cb3c5d
GASHIWINGO <117731094+GASHIWINGO@users.noreply.github.com>
2022-11-08 16:30:03 +0300

    Update 2.pas

--- 2.pas
+++ 2.pas
@@ -1,4 +1,4 @@
-﻿begin
+begin
   Println('Введите строку');
   var a:string;
   readln(a);
@@ -6,8 +6,13 @@
   var c,h:integer;
   for var i:=1 to b do
     begin
-      if a[i] = ' ' then Inc(c)
-      else Inc(h);
+      if a[i] = ' ' then Inc(c);
+      case a[i] of
+        'a'..'z': Inc(h);
+        'A'..'Z': Inc(h);
+        'а'..'я': Inc(h);
+        'А'..'я': Inc(h);
+      end;
     end;
   Println('Колличество букв в строке =', h);
   if b > 0 then Println('Колличество слов в строке =', c+1)
