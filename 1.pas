4193e49fc544a950287155429f26d1e079612972
GASHIWINGO <117731094+GASHIWINGO@users.noreply.github.com>
2022-11-08 14:22:27 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var a:array[1..10] of integer;
+  writeln('Введите 10 элементов массива');
+  for var b:=1 to 10 do 
+  begin
+    Readln(a[b]);
+  end;
+  Println('Массив:',a);
+  var s:integer;
+  for var b:=1 to 10 do s:=s+a[b];
+  Print('Среднеарифметическое в массиве =', s/10)
+end.
---
+++ 2.pas
@@ -0,0 +1,14 @@
+﻿begin
+  Println('Введите строку');
+  var a:string;
+  readln(a);
+  var b:=length(a);
+  var c,h:integer;
+  for var i:=1 to b do
+    begin
+      if a[i] = ' ' then Inc(c)
+      else Inc(h);
+    end;
+  Println('Колличество букв в строке =', h);
+  if b > 0 then Println('Колличество слов в строке =', c+1)
+end.
