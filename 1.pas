b61ed75759276c7db69cefec49c6b1a9e030aaaf
GASHIWINGO <117731094+GASHIWINGO@users.noreply.github.com>
2022-11-08 16:29:43 +0300

    Update 1.pas

--- 1.pas
+++ 1.pas
@@ -1,12 +1,13 @@
-﻿begin
-  var a:array[1..10] of integer;
+const n=10;
+begin
+  var a:array[1..n] of integer;
   writeln('Введите 10 элементов массива');
-  for var b:=1 to 10 do 
+  for var b:=1 to n do 
   begin
     Readln(a[b]);
   end;
   Println('Массив:',a);
   var s:integer;
-  for var b:=1 to 10 do s:=s+a[b];
-  Print('Среднеарифметическое в массиве =', s/10)
+  for var b:=1 to n do s:=s+a[b];
+  Print('Среднеарифметическое в массиве =', s/n)
 end.
